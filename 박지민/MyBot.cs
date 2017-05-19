98828f25f5a0c9ab8158c396b3a4c973e36a0526
Amanda <Amanda@JEONHYEJIN>
2017-05-18 23:36:03 -0500

    Add project files.

---
+++ 박지민/MyBot.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 박지민
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("help")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("For any questions or problems, please message @jeonhyejin^^");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzE0OTM0ODE3ODQwMDM3ODg4.DAAHRw.vuUEeukwAcnFBvbRN2YmAqbJIVk", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
